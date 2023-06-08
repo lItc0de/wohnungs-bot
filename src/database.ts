0bb374c59623f29c9b76ea5f795a8e78473f8404
litc0de <litc0de@posteo.org>
2023-06-08 19:56:53 +0200

    fix database query

--- src/database.ts
+++ src/database.ts
@@ -16,7 +16,8 @@ export default class DataBase {
 	}
 
 	private async migrate() {
-		if (DataBase.dbMigrateVersion <=1) await this.migrate_1();
+		if (DataBase.dbMigrateVersion <= 1) await this.migrate_1();
+		if (DataBase.dbMigrateVersion <= 2) await this.migrate_2();
 	}
 
 	private async migrate_1() {
@@ -65,12 +66,20 @@ export default class DataBase {
 		}
 	}
 
+	private async migrate_2() {
+		await this.sql.queryArray`
+		ALTER TABLE flats
+		DROP CONSTRAINT flats_company_key;
+		`
+	}
+
 	async createOffer(company: string, offer: Offer) {
 		const id = crypto.randomUUID();
 
 		await this.sql.queryArray`
-			INSERT OR IGNORE INTO flats (id, company, rent, size, rooms, url)
+			INSERT IGNORE INTO flats (id, company, rent, size, rooms, url)
 			VALUES ('${id}', '${company}', '${offer.rent}', '${offer.size}', '${offer.rooms}', '${offer.url}')
+			ON CONFLICT (url) DO NOTHING;
 		`;
 	}
 
